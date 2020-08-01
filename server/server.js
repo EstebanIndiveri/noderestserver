c9b49a585d7bf7a82d8313eb7059b543343c7fa7
Esteban Indiveri <esteban.indiveri@gmail.com>
2020-08-01 17:14:14 -0300

    routes change

--- server/server.js
+++ server/server.js
@@ -1,40 +1,20 @@
 require('./config/config');
 const express=require('express');
+const mongoose=require('mongoose');
 const app=express();
 const bodyparser=require('body-parser');
 
-app.use(bodyparser.urlencoded({extended:true}));
+app.use(bodyparser.urlencoded({extended:true,}));
 
 app.use(bodyparser.json());
 
-app.get('/',(req,res)=>{
-    res.send('hello');
-});
-app.get('/usuarios',(req,res)=>{
-    res.json('get usuario');
-});
+app.use(require('./routes/usuario'));
 
-app.post('/usuario',(req,res)=>{
-    let body=req.body;
-    if(body.nombre===undefined){
-        res.status(404).json({
-            ok:false,
-            message:'nombre necesario'
-        });
-    }else{
-        res.json(body);
-    }
-});
 
-app.put('/usuario/:id',(req,res)=>{
-    let id=req.params.id;
-    res.json({
-        id,
-        nombre:'user'
-    });
-});
-app.delete('/usuario',(req,res)=>{
-    res.json('delete usuario');
+
+mongoose.connect('mongodb://localhost:27017/coffetime',{ useNewUrlParser: true,useUnifiedTopology: true,useCreateIndex: true,useFindAndModify: false},(err,res)=>{
+    if(err)throw err;
+    console.log('base de datos UP');
 });
 
 app.listen(process.env.PORT,()=>{
